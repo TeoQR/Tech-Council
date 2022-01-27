d2dbfad2654f68363d60b40e3b3feb12cc81a773
thegrimbee <gabriel.mario.antaputra@gmail.com>
2022-01-27 17:09:11 +0800

    Create Starting Guide.cpp

---
+++ Reference/Starting Guide.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h> //allows you to use many useful functions
+using namespace std; //just add it, it makes your life better
+
+int main(){
+  int number1; //declaring a variable called number1 which is an integer
+
+  int number2, number3, number4; //it is possible to declaring multiple variables at once as long as they're the same data type
+
+  int number5 = 100; //you can also initiate a value for the variables
+  
+}
