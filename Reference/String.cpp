9b846a96e59712bf473422c72835d5ed963f3749
thegrimbee <gabriel.mario.antaputra@gmail.com>
2022-02-13 20:11:05 +0800

    Update String.cpp

--- Reference/String.cpp
+++ Reference/String.cpp
@@ -11,7 +11,7 @@ int main(){
   cout << s1[s1.size() - 1]; //s.size() - 1 is the last index of string s1, this will output o 
   //the output would be hello, cout << s1 would also output hello
   string s2 = "world";
-  string s3 = s1 + ' ' s2 + '!'; //The + operator combines strings together, also remember that single characters must use the single quotes '' instead of the double quotes ""
+  string s3 = s1 + ' ' + s2 + '!'; //The + operator combines strings together, also remember that single characters must use the single quotes '' instead of the double quotes ""
   for (auto letter : s3){
     cout << x << ' '; //The auto will convert letter into a char, the output would be h e l l o   w  o r l d !
   }
