b5b1f7866d5cb28118dde16e469738316f18c84d
thegrimbee <gabriel.mario.antaputra@gmail.com>
2022-05-23 09:17:16 +0800

    Update String.cpp

--- Reference/String.cpp
+++ Reference/String.cpp
@@ -4,9 +4,9 @@ using namespace std;
 int main(){
   char x = 'a'; //char is a data type which stores a single character, the character must be enveloped with the ' '
   cout << (int)x; //converting char to int will return the ASCII code, in this case, the output would be 97
-  string s1 = "hello"; // the ""
+  string s1 = "hello"; // for strings, you need to envelop the letters with " "
   for (int i = 0; i < s1.size(); ++i){ //.size() returns the length of the string
-    cout << s1[i];  //string is similar to an array of char, accessing the elements of the string can be done with the [] just like arrays
+    cout << s1[i];  //string is similar to an array of char, accessing the elements of the string can be done with the [] just like arrays (refer to arrays section)
   }
   cout << s1[s1.size() - 1]; //s.size() - 1 is the last index of string s1, this will output o 
   //the output would be hello, cout << s1 would also output hello
