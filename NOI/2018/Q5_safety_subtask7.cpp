a674ae2b5baaff63f93e64d96aebf27283c3d2b8
thegrimbee <gabriel.mario.antaputra@gmail.com>
2023-03-10 21:05:37 +0800

    Update Q5_safety_subtask7.cpp

--- NOI/2018/Q5_safety_subtask7.cpp
+++ NOI/2018/Q5_safety_subtask7.cpp
@@ -5,13 +5,14 @@ using namespace std;
 
 signed main()
 {
+//  input
     int n, h; cin >> n >> h;
     int m = 0;
     int heights[n];
     deque<pair<int, int>> dq;
     for (int i = 0; i < n; ++i){
         cin >> heights[i];
-        m = max(m, heights[i]);
+        m = max(m, heights[i]); // figuring out max height
     }
     if(h >= m)cout << 0;
     else{
@@ -23,21 +24,22 @@ signed main()
                 }
             }
             else{
+//              using a sliding range minimum query
                 dq = deque<pair<int, int>>();
                 for (int j = 0; j <= h; ++j){
-                    while(!dq.empty() && dp[i-1][j] <= dq.back().first){dq.pop_back();}
+                    while(!dq.empty() && dp[i-1][j] <= dq.back().first){dq.pop_back();} //maintaining a strictly increasing deque
                     dq.push_back(make_pair(dp[i-1][j], j));
                 }
-                for (int j = 0; j <= m; ++j){
+                for (int j = 0; j <= m; ++j){ 
                     pair<int, int> temp = dq.front();
 //                    cout << i << ' ' << j << ' ' << temp.first << ' ' << temp.second << '\n';
-                    dp[i][j] = temp.first + abs(heights[i]-j);
+                    dp[i][j] = temp.first + abs(heights[i]-j); // calculation of dp[i][j]
                     if(j != m){
-                        if(j < m-h){
-                            while(!dq.empty() && dp[i-1][j+1+h] <= dq.back().first){dq.pop_back();}
+                        if(j < m-h){ // if new items can still be added to deque
+                            while(!dq.empty() && dp[i-1][j+1+h] <= dq.back().first){dq.pop_back();} //maintaining a strictly increasing deque
                             dq.push_back(make_pair(dp[i-1][j+1+h], j+1+h));
                         }
-                        while(j+1-h>dq.front().second){dq.pop_front();}
+                        while(j+1-h>dq.front().second){dq.pop_front();} //removing expired items
                     }
                 }
             }
