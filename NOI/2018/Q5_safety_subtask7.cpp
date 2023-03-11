c123a3598715e6222187fd2c3232d174b02e650f
thegrimbee <gabriel.mario.antaputra@gmail.com>
2023-03-11 10:59:47 +0800

    Update Q5_safety_subtask7.cpp

--- NOI/2018/Q5_safety_subtask7.cpp
+++ NOI/2018/Q5_safety_subtask7.cpp
@@ -31,8 +31,7 @@ signed main()
                     dq.push_back(make_pair(dp[i-1][j], j));
                 }
                 for (int j = 0; j <= m; ++j){ 
-                    pair<int, int> temp = dq.front();
-//                    cout << i << ' ' << j << ' ' << temp.first << ' ' << temp.second << '\n';
+                    pair<int, int> temp = dq.front();  //temp represents min(dp(i-1, j-h to j+h))
                     dp[i][j] = temp.first + abs(heights[i]-j); // calculation of dp[i][j]
                     if(j != m){
                         if(j < m-h){ // if new items can still be added to deque
@@ -61,3 +60,31 @@ signed main()
     }
     return 0;
 }
+
+
+/*
+n*(m+1) states
+dp(i, j) is the minimum number of blocks added/removed to set column i to height j while still ensuring that the artwork is safe from column 0 to i
+dp(2, 3)
+dp(2, 3) = min(dp(1, 3-h to 3+h)) + abs(heights[i] - j)
+dp(i, j) = min(dp(i-1, j-h to j+h)) + abs(heights[i] - j)
+dp(0,j) = abs(heights[i] - j)
+the time complexity of finding the minimum number of a set of numbers of length N is O(N)
+O(1)
+sliding range minimum query
+dp(2, 0)
+dp(2, 1)
+{5,4,6},5,8,10,9
+5,{4,6,5},8,10,9
+5,4,{6,7,5},10,9
+
+deque<pair<int, int>> dq = {[4,1], [5,3]}
+0 <= j <= m
+
+m is the maximum height out of all the columns
+if h >= m the artwork is guaranteed to be safe
+
+deque:
+-the front item is the minimum number within the range we are looking artwork
+-the deque is strictly increasing
+*/
