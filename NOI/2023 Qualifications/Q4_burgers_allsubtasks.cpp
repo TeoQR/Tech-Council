90e9ccdf46042621a73f68e21b8f70cb25748efe
thegrimbee <gabriel.mario.antaputra@gmail.com>
2023-03-11 08:30:23 +0800

    Update Q4_burgers_allsubtasks.cpp

--- NOI/2023 Qualifications/Q4_burgers_allsubtasks.cpp
+++ NOI/2023 Qualifications/Q4_burgers_allsubtasks.cpp
@@ -20,13 +20,10 @@ signed main()
     int lo1 = 0, hi1 = 1e9+1, mid1 = (lo1+hi1)/2;
     while (lo1 < mid1){
         int lo2 = 0, hi2 = mid1 + 1, mid2 = (lo2 + hi2)/2;
-//        cout << "phase 1 " << lo1 << ' ' << mid1 << ' ' << hi1 << '\n';
         while (lo2 < hi2){
-//            cout << "phase 2 " << lo2 << ' ' << mid2 << ' ' << hi2 << '\n';
             int temp = mid1-mid2;
             done = false, impossible = false;
             for (int i = 0; i < n; ++i){
-//                cout << i << ' ' << (a[i] * mid2) + (b[i] * temp) << '\n';
                 if(a[i] * mid2 + b[i] * temp > x[i]){
                     if(a[i]>b[i])hi2 = mid2;
                     else if (a[i] == b[i])impossible = true;
@@ -35,9 +32,6 @@ signed main()
                 }
                 else if (i == n-1)done = true;
             }
-//           if (done) cout << "done\n";
-//           if (impossible) cout << "impossible\n"; 
-
             if(impossible||done)break;
             mid2 = (lo2+hi2)/2;
         }
