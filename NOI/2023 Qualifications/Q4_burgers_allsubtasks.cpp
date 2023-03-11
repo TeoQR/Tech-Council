aced831647e7746f75c8fd95ea30bc9b1e514a93
thegrimbee <gabriel.mario.antaputra@gmail.com>
2023-03-11 08:36:41 +0800

    Update Q4_burgers_allsubtasks.cpp

--- NOI/2023 Qualifications/Q4_burgers_allsubtasks.cpp
+++ NOI/2023 Qualifications/Q4_burgers_allsubtasks.cpp
@@ -4,6 +4,7 @@ using namespace std;
 
 signed main()
 {
+// input
     int n; cin >> n;
     int a[n], b[n], x[n];
     for (int i = 0; i < n; ++i){
@@ -15,13 +16,12 @@ signed main()
     for (int i = 0; i < n; ++i){
         cin >> b[i];
     }
-    
     bool done, impossible;
-    int lo1 = 0, hi1 = 1e9+1, mid1 = (lo1+hi1)/2;
+    int lo1 = 0, hi1 = 1e9+1, mid1 = (lo1+hi1)/2; // mid1 is the guess for the total number of burgers made
     while (lo1 < mid1){
-        int lo2 = 0, hi2 = mid1 + 1, mid2 = (lo2 + hi2)/2;
+        int lo2 = 0, hi2 = mid1 + 1, mid2 = (lo2 + hi2)/2; // mid2 is the guess for the number of burgers using the first recipe
         while (lo2 < hi2){
-            int temp = mid1-mid2;
+            int temp = mid1-mid2; // the number of burgers using the second recipe
             done = false, impossible = false;
             for (int i = 0; i < n; ++i){
                 if(a[i] * mid2 + b[i] * temp > x[i]){
